ea0497103b95973eb8fd04e751b897ed13e6f322
asbyvshev <barseto4ka@live.ru>
2020-05-31 19:01:50 +0300

    Files. Добавлен метод findAll.

--- src/main/java/com/kropotov/asrd/repositories/FileRepository.java
+++ src/main/java/com/kropotov/asrd/repositories/FileRepository.java
@@ -4,5 +4,8 @@ import com.kropotov.asrd.entities.docs.File;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FileRepository extends CrudRepository<File, Long> {
+    List<File> findAll();
 }
