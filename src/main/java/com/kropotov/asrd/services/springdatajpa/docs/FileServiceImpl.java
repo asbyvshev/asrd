4043409e30deccd6b9cc80be76befb4e620d2dd4
asbyvshev <barseto4ka@live.ru>
2020-05-31 22:09:35 +0300

    Files. Создан Сервис для File с начальным функцианалом MinIO.

--- src/main/java/com/kropotov/asrd/services/springdatajpa/docs/FileServiceImpl.java
+++ src/main/java/com/kropotov/asrd/services/springdatajpa/docs/FileServiceImpl.java
@@ -6,6 +6,10 @@ import com.kropotov.asrd.repositories.FileRepository;
 import com.kropotov.asrd.services.FileService;
 
 
+import io.minio.MinioClient;
+import io.minio.PutObjectOptions;
+import io.minio.errors.*;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,15 +19,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
 
@@ -31,11 +40,46 @@ public class FileServiceImpl implements FileService {
     private Path storePath;
 
     private final FileRepository fileRepository;
+    private final MinioClient minioClient;
 
-    private void putFileInStorage(MultipartFile file, String fileName) throws IOException {
+    private void putFileInStorage(MultipartFile file, String fileName, String bucket) throws IOException {
         Path targetLocation = storePath.resolve(fileName);
         Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
         log.info("File {} has been succesfully uploaded!", fileName);
+
+        // TODO: 31.05.2020  MINIO
+        //        InputStream stream = file.getInputStream();
+        //        Long fileSize = file.getSize();
+        //        PutObjectOptions options = new PutObjectOptions(fileSize, fileSize);
+        //        try {
+        //            minioClient.putObject(bucket,fileName,stream,options);
+        //        } catch (ErrorResponseException e) {
+        //            e.printStackTrace();
+        //        } catch (InsufficientDataException e) {
+        //            e.printStackTrace();
+        //        } catch (InternalException e) {
+        //            e.printStackTrace();
+        //        } catch (InvalidBucketNameException e) {
+        //            e.printStackTrace();
+        //        } catch (InvalidKeyException e) {
+        //            e.printStackTrace();
+        //        } catch (InvalidResponseException e) {
+        //            e.printStackTrace();
+        //        } catch (NoSuchAlgorithmException e) {
+        //            e.printStackTrace();
+        //        } catch (XmlParserException e) {
+        //            e.printStackTrace();
+        //        }
+
+
+    }
+
+    private boolean removeFromStorage (Long id){
+        File file = fileRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Oops! Review " + id + " wasn't found!")
+        );
+
+        return true;
     }
 
     private String currentDate(){
@@ -47,7 +91,9 @@ public class FileServiceImpl implements FileService {
     private String determineFileExtension(MultipartFile file) {
 
         switch (Objects.requireNonNull(file.getContentType())) {
-// TODO: 28.05.2020  реализовать требуемые форматы файлов
+            case MediaType.APPLICATION_PDF_VALUE:
+                return "pdf";
+
             case MediaType.IMAGE_JPEG_VALUE:
                 return "jpeg";
 
@@ -63,10 +109,11 @@ public class FileServiceImpl implements FileService {
         }
     }
 
+
     public File uploadFile (MultipartFile file, String userFileName, FileType type) throws IOException {
         if (file.getBytes().length != 0) {
             String fileName = type.getDirectory() + currentDate() + "." + determineFileExtension(file);
-            putFileInStorage(file, fileName);
+            putFileInStorage(file, fileName, type.getDirectory());
             return save(new File(fileName, userFileName, type));
         } else {
             return null;
@@ -79,7 +126,7 @@ public class FileServiceImpl implements FileService {
         return fileRepository.findById(aLong);
     }
 
-    @Transactional
+
     @Override
     public File save(File object) {
         return fileRepository.save(object);
@@ -96,7 +143,8 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public void deleteById(Long aLong) {
-//        fileRepository.deleteById(aLong);
+        removeFromStorage(aLong);
+        fileRepository.deleteById(aLong);
     }
     // TODO: 28.05.2020
     //  выгрузка файла как картинки для представления на фронте  public BufferedImage loadFile(String id) {}
