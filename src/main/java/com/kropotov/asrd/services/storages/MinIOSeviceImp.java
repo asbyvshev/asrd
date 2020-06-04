8f5ce59a20a7fdc02afdbbd069bd320b6eb54786
asbyvshev <barseto4ka@live.ru>
2020-06-04 18:09:41 +0300

    FileServiceFasadImpl, MiniIOServiceImp написана реализация.

--- src/main/java/com/kropotov/asrd/services/storages/MinIOSeviceImp.java
+++ src/main/java/com/kropotov/asrd/services/storages/MinIOSeviceImp.java
@@ -2,12 +2,19 @@ package com.kropotov.asrd.services.storages;
 
 import com.kropotov.asrd.services.MinIOService;
 import io.minio.MinioClient;
+import io.minio.PutObjectOptions;
 import io.minio.Result;
-import io.minio.errors.XmlParserException;
+import io.minio.errors.*;
 import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 
 @Slf4j
@@ -18,8 +25,32 @@ public class MinIOSeviceImp implements MinIOService {
     private final MinioClient minioClient;
 
     @Override
-    public void uploadFile(String bucketname, String filename, byte[] content) {
+    public void uploadFile(MultipartFile file, String bucketname, String filename) throws IOException {
 
+        InputStream stream = file.getInputStream();
+        Long fileSize = file.getSize();
+        PutObjectOptions options = new PutObjectOptions(fileSize, fileSize);
+        try {
+            minioClient.putObject(bucketname,filename,stream,options);
+        } catch (ErrorResponseException e) {
+            e.printStackTrace();
+        } catch (InsufficientDataException e) {
+            e.printStackTrace();
+        } catch (InternalException e) {
+            e.printStackTrace();
+        } catch (InvalidBucketNameException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidResponseException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (XmlParserException e) {
+            e.printStackTrace();
+        }
+
+        log.info("File {} has been succesfully uploaded!", filename);
     }
 
     @Override
