8fb164514e79a3ef5fb694b8f559a6db54d0b874
asbyvshev <barseto4ka@live.ru>
2020-06-05 15:54:33 +0300

    Files. Проброс добавлен исключений.

--- src/main/java/com/kropotov/asrd/services/MinIOService.java
+++ src/main/java/com/kropotov/asrd/services/MinIOService.java
@@ -1,17 +1,20 @@
 package com.kropotov.asrd.services;
 
 import io.minio.Result;
+import io.minio.errors.*;
 import io.minio.messages.Item;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public interface MinIOService {
 
     void uploadFile(MultipartFile file, String bucketname, String filename) throws IOException;
-    byte[] downloadFile(String bucketname,String filename);
-    void deleteFile(String bucketname,String filename);
-    void makeBucket(String bucketname);
-    void removeBucket(String bucketname);
+    byte[] downloadFile(String bucketname,String filename) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException;
+    void deleteFile(String bucketname,String filename) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException;
+    void makeBucket(String bucketname) throws IOException, InvalidKeyException, InvalidResponseException, RegionConflictException, InsufficientDataException, NoSuchAlgorithmException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException;
+    void removeBucket(String bucketname) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException;
     Iterable<Result<Item>> listFiles(String bucketname);
 }
